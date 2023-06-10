ea5b7819255bcbfda0e4c08f0b52ecf567cf94f1
Aguinaldo Mendes de Oliveira junior <123522287+Aguinaldooli@users.noreply.github.com>
2023-06-09 21:37:31 -0300

    agoraconteudo

--- script.js
+++ script.js
@@ -1,71 +1,28 @@
-const images = document.querySelectorAll('.carousel img');
+// Obtém todas as imagens com a classe 'expandable-image'
+const images = document.querySelectorAll('.expandable-image');
 
-// Adiciona o evento de clique em cada imagem
-images.forEach(image => {
-    image.addEventListener('click', () => {
-        // Cria um elemento de imagem para exibir a imagem ampliada
-        const enlargedImage = document.createElement('img');
-        enlargedImage.src = image.src;
-        enlargedImage.classList.add('enlarged-image');
+// Obtém o modal, a imagem expandida e a legenda
+const modal = document.getElementById('imageModal');
+const expandedImage = document.getElementById('expandedImage');
+const caption = document.getElementById('caption');
 
-        // Cria o botão de fechar
-        const closeButton = document.createElement('button');
-        closeButton.innerHTML = 'Fechar';
-        closeButton.classList.add('close-button');
+// Percorre todas as imagens e adiciona um ouvinte de evento de clique
+images.forEach((image) => {
+  image.addEventListener('click', () => {
+    // Define a imagem expandida e a legenda
+    expandedImage.src = image.src;
+    caption.innerHTML = image.alt;
 
-        // Cria uma div para envolver a imagem ampliada e o botão de fechar
-        const overlay = document.createElement('div');
-        overlay.classList.add('overlay');
-        overlay.appendChild(closeButton);
-        overlay.appendChild(enlargedImage);
-
-        // Adiciona a div de sobreposição ao corpo do documento
-        document.body.appendChild(overlay);
-
-        // Remove a div de sobreposição ao clicar no botão de fechar
-        closeButton.addEventListener('click', () => {
-            document.body.removeChild(overlay);
-        });
-    });
+    // Abre o modal
+    modal.style.display = 'block';
+  });
 });
 
-window.addEventListener('load', function() {
-  var carouselImages = document.querySelectorAll('.subject#automacao .carousel img');
-  var windowHeight = window.innerHeight || document.documentElement.clientHeight;
-  var inView = [];
+// Obtém o botão de fechar o modal
+const closeButton = document.querySelector('.close-button');
 
-  function isElementVisible(element) {
-    var rect = element.getBoundingClientRect();
-    return rect.top < windowHeight && rect.bottom >= 0;
-  }
-
-  function updateCarouselImages() {
-    inView = [];
-    carouselImages.forEach(function(image) {
-      if (isElementVisible(image)) {
-        inView.push(image);
-      }
-    });
-
-    carouselImages.forEach(function(image) {
-      image.classList.remove('active');
-    });
-
-    if (inView.length > 0) {
-      inView[inView.length - 1].classList.add('active');
-    }
-  }
-
-  function handleScroll() {
-    var automacaoSection = document.querySelector('#automacao');
-    if (isElementVisible(automacaoSection)) {
-      document.body.classList.add('no-scroll');
-      updateCarouselImages();
-    } else {
-      document.body.classList.remove('no-scroll');
-    }
-  }
-
-  window.addEventListener('scroll', handleScroll);
-  window.addEventListener('resize', handleScroll);
+// Adiciona um ouvinte de evento de clique no botão de fechar
+closeButton.addEventListener('click', () => {
+  // Fecha o modal
+  modal.style.display = 'none';
 });
