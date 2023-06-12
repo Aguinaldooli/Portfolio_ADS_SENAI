eb0f2bc4032d76c0aa4f77dcf3ac3f7a89e99054
Aguinaldo Mendes de Oliveira junior <123522287+Aguinaldooli@users.noreply.github.com>
2023-06-12 00:29:17 -0300

    newconted
    atualizando texto e conteudo

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 // Obtém todas as imagens com a classe 'expandable-image'
-const images = document.querySelectorAll('.expandable-image');
+const images = document.querySelectorAll(".expandable-image");
 
 // Obtém o modal, a imagem expandida e a legenda
-const modal = document.getElementById('imageModal');
-const expandedImage = document.getElementById('expandedImage');
-const caption = document.getElementById('caption');
+const modal = document.getElementById("imageModal");
+const expandedImage = document.getElementById("expandedImage");
+const caption = document.getElementById("caption");
 
 // Percorre todas as imagens e adiciona um ouvinte de evento de clique
 images.forEach((image) => {
-  image.addEventListener('click', () => {
+  image.addEventListener("click", () => {
     // Define a imagem expandida e a legenda
     expandedImage.src = image.src;
     caption.innerHTML = image.alt;
 
     // Abre o modal
-    modal.style.display = 'block';
+    modal.style.display = "block";
   });
 });
 
 // Obtém o botão de fechar o modal
-const closeButton = document.querySelector('.close-button');
+const closeButton = document.querySelector(".close-button");
 
 // Adiciona um ouvinte de evento de clique no botão de fechar
-closeButton.addEventListener('click', () => {
+closeButton.addEventListener("click", () => {
   // Fecha o modal
-  modal.style.display = 'none';
+  modal.style.display = "none";
 });
